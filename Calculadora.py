efd4f0a97ed8e7fd8ceadb4f5ba1f07648ffa32e
Erick14911 <erickdlr14911@gmail.com>
2022-07-09 08:34:34 -0500

    Operaciones

--- Calculadora.py
+++ Calculadora.py
@@ -3,13 +3,22 @@ def suma(a,b):
     c=a+b
     return c
 def resta():
-    
+
+def resta(a,b):
+    c=a-B
+    return c
+def multiplica(a,b):
+    c=a*b 
+    return c
+def divixion(a,b):
+    c=a/b 
+    return c    
 
 print("Suma los numeros")
 #num1=float(input("Ingrasa A: "))
 #num2=float(input("Ingresa B: "))
-num1=int(sys.argv[1])
-num2=int(sys.argv[2])
+num1=float(sys.argv[1])
+num2=float(sys.argv[2])
 
     
 resultado=suma(num1,num2)    
